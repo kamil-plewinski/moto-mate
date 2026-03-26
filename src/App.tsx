60b689c2164c8f7ca0ad169519b8090409402c44
kamol plew <kplewinski7@gmail.com>
2026-03-26 16:50:14 +0100

    feat: add React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
-import Header from "./components/Header";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./pages/Root";
+import HomePage from "./pages/HomePage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      // {
+      //   path: "/my-vehicles",
+      //   element: <MyVehicles />,
+      // },
+    ],
+  },
+]);
 
 export default function App() {
-  return <Header/>;
+  return <RouterProvider router={router} />;
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
 import LogoIcon from "./icons/LogoIcon";
 
 export default function Header() {
   return (
     <header className="flex items-center w-full bg-gray-500">
-      <LogoIcon />
-      <h1 className="pl-[2em] text-xl">MotoMate</h1>
+      <Link to="/">
+        <LogoIcon />
+      </Link>
     </header>
   );
 }
