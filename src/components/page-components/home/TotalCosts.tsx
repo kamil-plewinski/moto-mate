909d05844e41a6e82fa7419b5a5c8f0ee8698a34
kamol plew <kplewinski7@gmail.com>
2026-03-30 09:36:32 +0200

    refactor: replace multiple home stats components with reusable InfoCard

--- src/components/page-components/home/TotalCosts.tsx
+++
@@ -1,7 +0,0 @@
-export default function TotalCosts() {
-  return (
-    <div className="custom-background w-full h-34 lg:col-[2/_span_1] lg:row-[1/_span_1]">
-      <h2>Całkowite koszty</h2>
-    </div>
-  );
-}
