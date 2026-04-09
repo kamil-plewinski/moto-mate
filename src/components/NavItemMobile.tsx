f9f1d929302651f0a0a033735801a7caba5c3124
Kamil <kplewinski7@gmail.com>
2026-04-09 15:00:18 +0200

    refactor: replace div and p with span in NavItemMobile for better semantics

--- src/components/NavItemMobile.tsx
+++ src/components/NavItemMobile.tsx
@@ -22,8 +22,8 @@ export default function NavItemMobile({
           `flex flex-col items-center justify-center w-25 transition-all duration-300  ${isActive ? "text-[rgb(202,28,22)] drop-shadow-[0px_3px_5px_rgba(153,52,52,0.9)] scale-105" : "text-[rgba(221,221,221,.8)] scale-100"}`
         }
       >
-        <div className="mb-2">{icon}</div>
-        <p>{title}</p>
+        <span className="mb-2">{icon}</span>
+        <span>{title}</span>
       </NavLink>
     </div>
   );
