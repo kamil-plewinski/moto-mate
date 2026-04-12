2f0449c491fff6e468dedb7289dfe014940b3401
Kamil <kplewinski7@gmail.com>
2026-04-12 14:07:12 +0200

    feat: add vehicle form modal with dynamic config per vehicle type

--- src/components/page-components/add-vehicle/AddVehicleBtn.tsx
+++ src/components/page-components/add-vehicle/AddVehicleBtn.tsx
@@ -1,11 +1,13 @@
 type AddVehicleBtnProps = {
   name: string;
+  onClick: () => void;
 };
 
-export default function AddVehicleBtn({ name }: AddVehicleBtnProps) {
+export default function AddVehicleBtn({ name, onClick }: AddVehicleBtnProps) {
   return (
     <button
       type="button"
+      onClick={onClick}
       className="w-full max-w-175 h-45 bg-red-300 cursor-pointer md:h-60 lg:max-w-120 lg:h-150"
     >
       {name}
