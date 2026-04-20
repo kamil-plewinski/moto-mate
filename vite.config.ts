c53bc19b99c28608ea9da19f04fa9c97ecf95307
Kamil <kplewinski7@gmail.com>
2026-04-20 20:28:43 +0200

    fix: prevent Vite HMR reload caused by db.json changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 3000,
+    watch: {
+      ignored: ["**/db.json"],
+    },
+  },
+  build: {
+    target: "esnext",
+  },
 });
