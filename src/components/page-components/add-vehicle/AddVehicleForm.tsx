e6cfcbaa1b9c5aa57a75cb8932929e3a735070d6
Kamil <kplewinski7@gmail.com>
2026-06-08 09:41:09 +0200

    feat:add vehicle photo search UI

--- src/components/page-components/add-vehicle/AddVehicleForm.tsx
+++ src/components/page-components/add-vehicle/AddVehicleForm.tsx
@@ -80,93 +80,95 @@ export default function AddVehicleForm({
   };
 
   return (
-    <form
-      onClick={(e) => e.stopPropagation()}
-      onSubmit={submitForm}
-      className="relative w-full mx-2 flex flex-col items-center justify-center px-10 py-9 custom-background rounded-xl text-sm shadow-md md:mx-10 md:text-md lg:flex-row-reverse lg:justify-around lg:gap-6 lg:max-w-300 lg:py-30 xl:py-36 xl:px-14"
-    >
-      <button
-        type="button"
-        onClick={closeForm}
-        className="absolute top-0 right-0 m-3 cursor-pointer md:m-6 "
+    <>
+      <form
+        onClick={(e) => e.stopPropagation()}
+        onSubmit={submitForm}
+        className="relative w-full mx-2 flex flex-col items-center justify-center px-10 py-9 custom-background rounded-xl text-sm shadow-md md:mx-10 md:text-md lg:flex-row-reverse lg:justify-around lg:gap-6 lg:max-w-300 lg:py-30 xl:py-36 xl:px-14"
       >
-        <X
-          size={34}
-          className="text-gray-200 hover:text-[#D71F1F] transition-colors duration-200"
-        />
-      </button>
-      <div>
-        <img
-          id="vehicle-photo"
-          src={config.defaultPhoto}
-          alt="Zdjęcie wybranego pojazdu"
-          className="mt-4 w-full max-w-100 shadow-md rounded-md md:max-w-120 lg:mt-0"
-        />
-      </div>
-      <div className="my-5 max-w-120 md:max-w-140 lg:max-w-120 lg:my-0">
-        <label htmlFor="brand">Marka:</label>
-        <input
-          id="brand"
-          name="brand"
-          type="text"
-          placeholder={config.brandPlaceholder}
-          className={inputClasses}
-          required
-        />
-        <label htmlFor="model" className={labelClasses}>
-          Model:
-        </label>
-        <input
-          id="model"
-          name="model"
-          type="text"
-          placeholder={config.modelPlaceholder}
-          className={inputClasses}
-          required
-        />
-        <label htmlFor="year" className={labelClasses}>
-          Rok produkcji:
-        </label>
-        <input
-          id="year"
-          name="year"
-          type="number"
-          placeholder={config.yearPlaceholder}
-          className={inputClasses}
-          required
-        />
-        <label htmlFor="odometer" className={labelClasses}>
-          Przebieg:
-        </label>
-        <input
-          id="odometer"
-          name="odometer"
-          type="number"
-          placeholder={config.odometerPlaceholder}
-          className={inputClasses}
-          required
-        />
-        <label className={labelClasses}>Silnik:</label>
-        <input
-          id="engine"
-          name="engine"
-          placeholder={config.enginePlaceholder}
-          className={inputClasses}
-          required
-        />
-      </div>
-      <div className="flex gap-4 items-center justify-center w-full sm:gap-10 md:gap-20 lg:absolute lg:bottom-0 lg:mb-8">
         <button
-          type="submit"
-          className="py-3 w-37 text-md uppercase font-semibold  rounded-md bg-linear-to-br from-[#993434] to-[#D71F1F] cursor-pointer hover:from-[#D71F1F] hover:to-[#993434] transition-colors duration-300 shadow-md"
+          type="button"
+          onClick={closeForm}
+          className="absolute top-0 right-0 m-3 cursor-pointer md:m-6 "
         >
-          Dodaj
+          <X
+            size={34}
+            className="text-gray-200 hover:text-[#D71F1F] transition-colors duration-200"
+          />
         </button>
-        <VehiclePhotoBtn togglePhotoModal={togglePhotoModal} />
-      </div>
+        <div>
+          <img
+            id="vehicle-photo"
+            src={config.defaultPhoto}
+            alt="Zdjęcie wybranego pojazdu"
+            className="mt-4 w-full max-w-100 shadow-md rounded-md md:max-w-120 lg:mt-0"
+          />
+        </div>
+        <div className="my-5 max-w-120 md:max-w-140 lg:max-w-120 lg:my-0">
+          <label htmlFor="brand">Marka:</label>
+          <input
+            id="brand"
+            name="brand"
+            type="text"
+            placeholder={config.brandPlaceholder}
+            className={inputClasses}
+            required
+          />
+          <label htmlFor="model" className={labelClasses}>
+            Model:
+          </label>
+          <input
+            id="model"
+            name="model"
+            type="text"
+            placeholder={config.modelPlaceholder}
+            className={inputClasses}
+            required
+          />
+          <label htmlFor="year" className={labelClasses}>
+            Rok produkcji:
+          </label>
+          <input
+            id="year"
+            name="year"
+            type="number"
+            placeholder={config.yearPlaceholder}
+            className={inputClasses}
+            required
+          />
+          <label htmlFor="odometer" className={labelClasses}>
+            Przebieg:
+          </label>
+          <input
+            id="odometer"
+            name="odometer"
+            type="number"
+            placeholder={config.odometerPlaceholder}
+            className={inputClasses}
+            required
+          />
+          <label className={labelClasses}>Silnik:</label>
+          <input
+            id="engine"
+            name="engine"
+            placeholder={config.enginePlaceholder}
+            className={inputClasses}
+            required
+          />
+        </div>
+        <div className="flex gap-4 items-center justify-center w-full sm:gap-10 md:gap-20 lg:absolute lg:bottom-0 lg:mb-8">
+          <button
+            type="submit"
+            className="py-3 w-37 text-md uppercase font-semibold  rounded-md bg-linear-to-br from-[#993434] to-[#D71F1F] cursor-pointer hover:from-[#D71F1F] hover:to-[#993434] transition-colors duration-300 shadow-md"
+          >
+            Dodaj
+          </button>
+          <VehiclePhotoBtn togglePhotoModal={togglePhotoModal} />
+        </div>
+      </form>
       {isPhotoModalOpen && (
         <VehiclePhotoModal togglePhotoModal={togglePhotoModal} />
       )}
-    </form>
+    </>
   );
 }
--- src/components/page-components/add-vehicle/VehiclePhotoModal.tsx
+++ src/components/page-components/add-vehicle/VehiclePhotoModal.tsx
@@ -1,4 +1,5 @@
 import { X } from "lucide-react";
+import { useRef } from "react";
 
 type VehiclePhotoModalProps = {
   togglePhotoModal: () => void;
@@ -7,8 +8,17 @@ type VehiclePhotoModalProps = {
 export default function VehiclePhotoModal({
   togglePhotoModal,
 }: VehiclePhotoModalProps) {
+  const searchInput = useRef<HTMLInputElement>(null);
+
+  const handleSearch = () => {
+    console.log(searchInput.current?.value);
+  };
+
   return (
-    <div className="absolute top-[50%] translate-y-[-50%] w-90 h-126 bg-zinc-200">
+    <div
+      className="absolute top-[50%] translate-y-[-50%] p-4 w-[90vw] max-w-225 h-140 bg-zinc-200 text-zinc-800 rounded-xl shadow-md xl:h-150"
+      onClick={(e) => e.stopPropagation()}
+    >
       <button
         type="button"
         onClick={togglePhotoModal}
@@ -19,6 +29,32 @@ export default function VehiclePhotoModal({
           className="text-zinc-800 hover:text-[#D71F1F] transition-colors duration-200 mr-1"
         />
       </button>
+      <div className=" text-lg">
+        <p className="text-2xl font-semibold">Wyszukaj zdjęcie</p>
+        <div className="mt-6">
+          <div className="w-full flex items-center justify-center mt-4 ">
+            <input
+              type="search"
+              placeholder="Wpisz nazwę pojazdu"
+              ref={searchInput}
+              required
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  handleSearch();
+                }
+              }}
+              className="p-3 w-full max-w-100 bg-zinc-300 border border-zinc-400 focus:outline-[#993434] text-zinc-800 rounded-md "
+            />
+            <button
+              type="button"
+              onClick={handleSearch}
+              className="ml-4 py-2 px-3 w-25 uppercase font-semibold  rounded-md bg-linear-to-br from-[#993434] to-[#D71F1F] cursor-pointer hover:from-[#D71F1F] hover:to-[#993434] transition-colors duration-300 shadow-md text-white"
+            >
+              Szukaj
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
