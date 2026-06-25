ab23c93c99ce7c71ddebab6eace24aeea367f123
Kamil <kplewinski7@gmail.com>
2026-06-25 20:05:33 +0200

    refactor: move vehicle state to expenses page

--- src/components/page-components/expenses/VehicleSelector.tsx
+++ src/components/page-components/expenses/VehicleSelector.tsx
@@ -1,81 +1,37 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import CarIcon from "../../icons/CarIcon";
-import { getVehicles, updateVehicle } from "../../../api/vehicleApi";
 import type { VehicleType } from "../my-vehicles/vehicleType";
-import { usePopup } from "../../popup/usePopup";
 
-export default function VehicleSelector() {
+type VehicleSelectorProps = {
+  setActiveVehicle: (id: number, selectedVehicle: VehicleType) => void;
+  vehiclesList: VehicleType[];
+  activeVehicle: VehicleType | undefined;
+};
+
+export default function VehicleSelector({
+  vehiclesList,
+  setActiveVehicle,
+  activeVehicle,
+}: VehicleSelectorProps) {
   const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
-  const [vehiclesList, setVehiclesList] = useState<VehicleType[]>([]);
-
-  const { showPopup } = usePopup();
 
   const toggleDropdown = () => {
     setIsDropdownOpen((prev) => !prev);
   };
 
-  useEffect(() => {
-    const fetchVehicles = async () => {
-      try {
-        const data = await getVehicles();
-
-        setVehiclesList(data);
-      } catch (err) {
-        console.error(err, "Nie udało się pobrać listy pojazdów");
-      }
-    };
-
-    fetchVehicles();
-  }, []);
-
   const renderedVehiclesList = vehiclesList.map((vehicle) => {
+    const handleVehicleSelect = () => {
+      setActiveVehicle(vehicle.id, vehicle);
+      toggleDropdown();
+    };
     return (
       <li key={vehicle.id}>
-        <button
-          onClick={() => setActiveVehicle(vehicle.id, vehicle)}
-          className="my-1 cursor-pointer"
-        >
+        <button onClick={handleVehicleSelect} className="my-1 cursor-pointer">
           {vehicle.brand} {vehicle.model}
         </button>
       </li>
     );
   });
-
-  const setActiveVehicle = async (id: number, selectedVehicle: VehicleType) => {
-    try {
-      await Promise.all(
-        vehiclesList.map((vehicle) => {
-          const isActive = vehicle.id === id;
-
-          return updateVehicle(vehicle.id, isActive);
-        }),
-      );
-      setVehiclesList((prev) => {
-        return prev.map((vehicle) => ({
-          ...vehicle,
-          isActive: vehicle.id === id,
-        }));
-      });
-      toggleDropdown();
-      showPopup(
-        `Aktywny pojazd został zmieniony na ${selectedVehicle.brand} ${selectedVehicle.model}.`,
-        "favourite",
-      );
-    } catch (err) {
-      showPopup(
-        "Wystąpił błąd. Nie udało się wybrać aktywnego pojazdu.",
-        "error",
-      );
-      console.error(
-        err,
-        `Nie udało się ustawić ${selectedVehicle.brand} jako aktywny pojazd`,
-      );
-    }
-  };
-
-  const activeVehicle: VehicleType | undefined = vehiclesList.find(
-    (vehicle) => vehicle.isActive === true,
-  );
 
   return (
     <div className="flex items-center gap-4 p-4 w-full h-30 custom-background">
--- src/pages/ExpensesPage.tsx
+++ src/pages/ExpensesPage.tsx
@@ -1,13 +1,74 @@
+import { useState, useEffect } from "react";
+import { getVehicles, updateVehicle } from "../api/vehicleApi";
+import { usePopup } from "../components/popup/usePopup";
 import AddExpense from "../components/page-components/expenses/AddExpense";
 import ExpensesHistory from "../components/page-components/expenses/ExpensesHistory";
 import VehicleSelector from "../components/page-components/expenses/VehicleSelector";
+import type { VehicleType } from "../components/page-components/my-vehicles/vehicleType";
 
 export default function ExpensesPage() {
+  const [vehiclesList, setVehiclesList] = useState<VehicleType[]>([]);
+
+  const { showPopup } = usePopup();
+
+  useEffect(() => {
+    const fetchVehicles = async () => {
+      try {
+        const data = await getVehicles();
+
+        setVehiclesList(data);
+      } catch (err) {
+        console.error(err, "Nie udało się pobrać listy pojazdów");
+      }
+    };
+
+    fetchVehicles();
+  }, []);
+
+  const setActiveVehicle = async (id: number, selectedVehicle: VehicleType) => {
+    try {
+      await Promise.all(
+        vehiclesList.map((vehicle) => {
+          const isActive = vehicle.id === id;
+
+          return updateVehicle(vehicle.id, isActive);
+        }),
+      );
+      setVehiclesList((prev) => {
+        return prev.map((vehicle) => ({
+          ...vehicle,
+          isActive: vehicle.id === id,
+        }));
+      });
+      showPopup(
+        `Aktywny pojazd został zmieniony na ${selectedVehicle.brand} ${selectedVehicle.model}.`,
+        "favourite",
+      );
+    } catch (err) {
+      showPopup(
+        "Wystąpił błąd. Nie udało się wybrać aktywnego pojazdu.",
+        "error",
+      );
+      console.error(
+        err,
+        `Nie udało się ustawić ${selectedVehicle.brand} jako aktywny pojazd`,
+      );
+    }
+  };
+
+  const activeVehicle: VehicleType | undefined = vehiclesList.find(
+    (vehicle) => vehicle.isActive === true,
+  );
+
   return (
     <>
       <h1>Wydatki</h1>
       <div className="flex flex-col items-center gap-6">
-        <VehicleSelector />
+        <VehicleSelector
+          vehiclesList={vehiclesList}
+          setActiveVehicle={setActiveVehicle}
+          activeVehicle={activeVehicle}
+        />
         <AddExpense />
         <ExpensesHistory />
       </div>
